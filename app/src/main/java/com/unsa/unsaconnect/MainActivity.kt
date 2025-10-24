98c576395c79fa45ea5e47f43d757bba39953be3
MarcoAntonioPdLA <mponcedeleona@unsa.edu.pe>
2025-10-24 01:10:33 -0500

    Barra superior y modificación de temas.

--- app/src/main/java/com/unsa/unsaconnect/MainActivity.kt
+++ app/src/main/java/com/unsa/unsaconnect/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.sp
 import com.unsa.unsaconnect.ui.theme.UnsaConnectTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,17 +28,41 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             UnsaConnectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = { NewsTopBar() }
+                    ) { innerPadding ->
+                    MainNewsFeed(modifier = Modifier.padding(innerPadding).fillMaxSize())
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NewsTopBar() {
+    CenterAlignedTopAppBar(
+        title = {Text(
+            text = "UNSA Connect",
+            color = MaterialTheme.colorScheme.onPrimary,
+            fontSize = 18.sp,
+            fontFamily = FontFamily.Default
+        )},
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        )
+    )
+}
+
+@Composable
+fun MainNewsFeed(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        return
+    }
+}
+
+/*
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -45,3 +78,4 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+*/
--- app/src/main/java/com/unsa/unsaconnect/ui/theme/Color.kt
+++ app/src/main/java/com/unsa/unsaconnect/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package com.unsa.unsaconnect.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
+val PrimaryDark = Color(0xFF000000)
+val OnPrimaryDark = Color(0xFFFFFFFF)
+val BackgroungDark = Color(0xFF181818)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
+val PrimaryLight = Color(0xFFFFFFFF)
+val OnPrimaryLight = Color(0xFF000000)
+val BackgroungLight = Color(0xFFE3E3E3)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
