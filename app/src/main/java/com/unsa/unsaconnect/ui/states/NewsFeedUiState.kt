712758b1851e6e00ac9c2a1917a9f67d8cd75bc1
MarcoAntonioPdLA <mponcedeleona@unsa.edu.pe>
2025-11-29 04:29:18 -0500

    Agregando posibilidad de poder filtrar noticias por categorías.

--- app/src/main/java/com/unsa/unsaconnect/ui/states/NewsFeedUiState.kt
+++ app/src/main/java/com/unsa/unsaconnect/ui/states/NewsFeedUiState.kt
@@ -1,10 +1,12 @@
 package com.unsa.unsaconnect.ui.states
 
-import com.unsa.unsaconnect.data.models.New
 import com.unsa.unsaconnect.data.models.NewsWithCategories
+import com.unsa.unsaconnect.data.models.Category
 
 data class NewsFeedUiState(
     val highlightedNews: List<NewsWithCategories> = emptyList(),
     val recentNews: List<NewsWithCategories> = emptyList(),
+    val categories: List<Category> = emptyList(),
+    val selectedCategoryId: Int? = null,
     val isLoading: Boolean = false
 )
